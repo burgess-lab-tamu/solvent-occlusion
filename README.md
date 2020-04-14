01af3b265cceabf45f1c04d66f697a33ff6ded3e
lyu18 <26318510+lyu18@users.noreply.github.com>
2020-04-14 15:55:06 -0500

    License, packaging, etc.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# solvent-occlusion
-Inferring "hot spot" residues on protein-protein interfaces by solvent occlusion
+# Solvent Occlusion
+
+Calculate percentage loss of solvent accessible surface area of each residue in a protein upon binding to its receptor, i.e. solvent occlusion. A useful feature for locating "hot spot" residues on PPI interfaces.
+
+## Usage
+
+```python
+import solvent_occlusion as so
+atoms = so.parse("path/to/pdb/file")
+occlusions = so.get_occlusions(atoms)
+```
+
+Please also refer to `examples/example.ipynb` for an example.
---
+++ setup.py
@@ -0,0 +1,32 @@
+# Created by Rui-Liang Lyu, April 14, 2020
+#
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="solvent-occlusion-lyu18",
+    version="0.1",
+    author="Rui-Liang Lyu",
+    author_email="lyu.chemistry@tamu.edu",
+
+    description="Calculating solvent occlusion of protein residues upon binding",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lyu18/solvent-occlusion",
+
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "numpy>=1.17",
+        "pandas>=0.24",
+        "tqdm>=4.39"
+    ]
+)
